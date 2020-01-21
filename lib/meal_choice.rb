96cd9e55f15758bb0f71560bc03efd3749f27bea
Julian Wakefield <julianwakefield@outlook.com>
2020-01-21 12:05:17 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,14 +1,33 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 
-
-
-def meal_choice(protein, veg1, veg2)
-
-
-  puts "What a nutritious meal!"
-  puts "A plate of meat with #{veg1} and #{veg2}."
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
-
-
+def meal_choice("broccoli", "macaroni")
+  puts "A plate of meat with broccoli and macaroni"
 end
+
+
+
+#def meal_choice(protein, veg1, veg2)
+#  puts "What a nutritious meal!"
+#  puts "A plate of meat with #{veg1} and #{veg2}."
+#  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+#end
+
+
+#require 'spec_helper'
+
+#describe '#meal_choice' do
+#  it 'should default to meat for the protein' do
+#    expect(meal_choice("broccoli", "macaroni")).to eq("A plate of meat with broccoli and macaroni.")
+#  end
+
+#  it 'should allow you to set a protein' do
+#    expect(meal_choice("broccoli", "macaroni", "tofu")).to eq("A plate of tofu with broccoli and macaroni.")
+#  end
+
+#  it 'should puts "What a nutritious meal!" and your order to the console' do
+#    expect($stdout).to receive(:puts).with("What a nutritious meal!")
+#    expect($stdout).to receive(:puts).with("A plate of meat with broccoli and macaroni.")
+#    meal_choice("broccoli", "macaroni")
+#  end
+#end
