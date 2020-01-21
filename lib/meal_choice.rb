e4fcb751a2922fab7f46fddb6ddb11cca568390c
Julian Wakefield <julianwakefield@outlook.com>
2020-01-21 11:01:41 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,4 +4,4 @@ veg1 = broccoli
 veg2 = macaroni
 protein = tofu
 
-meal_choice puts "A plate of #{protein} with #{veg1} and #{veg2}."
+meal_choice = "A plate of #{protein} with #{veg1} and #{veg2}."
