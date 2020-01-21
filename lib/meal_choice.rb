b39c95b7b5ccedc3d21fcaf9e1d13f53b2a442c7
Julian Wakefield <julianwakefield@outlook.com>
2020-01-21 11:05:26 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,7 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 
-def meal_choice(broccoli, macaroni) 
+def meal_choice(broccoli, macaroni)
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 
 veg1 = broccoli
@@ -9,4 +9,3 @@ veg2 = macaroni
 protein = meat
 
 end
-
