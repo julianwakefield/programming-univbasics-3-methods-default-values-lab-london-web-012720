b90796b71f017a5beb46582d58c88320c111db45
Julian Wakefield <julianwakefield@outlook.com>
2020-01-21 11:10:34 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,13 +3,13 @@
 
 
 
-def meal_choice(meat, broccoli, macaroni)
+def meal_choice(veg1, veg2, protein)
 
   veg1 = broccoli
   veg2 = macaroni
-  protein = meat
+  protein = tofu
 
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts "A plate of meat with #{veg1} and #{veg2}."
 
 
 end
